e91ce8dc32afb7c6ce53ea824470cb53b37fe6c4
JaveriaNoorMalick <javeria.noor.malick@gmail.com>
2016-05-29 13:52:07 +0000

    Add files via upload

---
+++ Drad n Drop/Drad n Drop/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Drad_n_Drop
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            String s = "Javeria";
+            
+            if (txtBx1.Text=="javeria")
+            {
+                MessageBox.Show("Hello To XAML world");
+            }
+            else
+            {
+                MessageBox.Show("You are not allowed");
+            }
+           
+
+        }
+    }
+}
